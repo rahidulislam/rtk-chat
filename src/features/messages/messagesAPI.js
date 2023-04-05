ab80a9c0b6961c1d5cae30e861fb4162e8dace7a
Rahidul Islam <rahiseli@outlook.com>
2023-04-05 19:04:11 +0600

    updated

--- src/features/messages/messagesAPI.js
+++ src/features/messages/messagesAPI.js
@@ -1,5 +1,12 @@
 import { apiSlice } from "../api/apiSlice"
 
 export const messagesAPI = apiSlice.injectEndpoints({
-    endpoints: (builder)=>{}
+    endpoints: (builder)=>({
+        getMessages: builder.query({
+            query:(id) =>
+            `/conversations?participants_like=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
+        })
+    })
 })
+
+export const {useGetMessagesQuery} = messagesAPI;
